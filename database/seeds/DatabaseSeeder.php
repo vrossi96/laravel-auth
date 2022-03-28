eac1db4bcbecbead7f5aefaaffb083adada660d3
vrossi96 <vrossi396@yahoo.it>
2022-03-28 16:56:32 +0200

    add admin user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([PostSeeder::class]);
+        $this->call([PostSeeder::class, UserSeeder::class]);
     }
 }
