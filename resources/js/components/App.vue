5a504e1e1b539152140c2e6889948312e4a70050
vrossi96 <vrossi396@yahoo.it>
2022-03-28 16:16:49 +0200

    add vue component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -3,10 +3,10 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">Work in Progress</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        Log in to start
                     </div>
                 </div>
             </div>
